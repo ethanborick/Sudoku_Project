c35696c9894f8bb53a0cd30243b808627a0239d8
ethanborick <eborick@ufl.edu>
2025-04-15 17:15:07 -0400

    add more of the functions

--- board.py
+++ board.py
@@ -8,7 +8,8 @@ class Board:
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
-        self.cells = [[Cell(0, i , j, screen) for j in range(BOARD_ROWS)] for i in range(BOARD_COLS)]
+        self.cells = [[Cell(0, i , j, screen) for j in range(BOARD_COLS)] for i in range(BOARD_ROWS)]
+        self.selected_cell = None
     def draw(self):
         for i in range(BOARD_ROWS + 1):
             line_thickness = BOLD_LINE_WIDTH if i % 3 == 0 else LINE_WIDTH
@@ -33,6 +34,26 @@ class Board:
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
                 self.cells[i][j].draw()
+    def select(self, row, col):
+        self.cells[row][col].selected = True
+        self.cells[row][col].draw()
+        self.selected_cell = self.cells[row][col]
+    def click(self, x, y):
+        if 0 <= x <= WIDTH and 0 <= y <= HEIGHT:
+            row = y // SMALL_SQUARE_SIZE
+            col = x // SMALL_SQUARE_SIZE
+            return (row, col)
+        return None
+    def clear(self, row, col):
+        if self.cells[row][col].value != 0:
+            self.cells[row][col].set_cell_value(0)
+    def sketch(self, value):
+        if not self.selected_cell.given_value:
+            self.selected_cell.set_sketched_value(value)
+
+
+
+
 
 
 
